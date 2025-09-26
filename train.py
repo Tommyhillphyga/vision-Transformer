33854ba89771d41d1894e0d8be641b35d899e00f
TOMIWA <tommyhillphyga@gmail.com>
2025-09-26 14:27:39 +0100

    update vit

--- train.py
+++ train.py
@@ -14,8 +14,6 @@ from vit import Vit
 np.random.seed(0)
 torch.manual_seed(0)
 
-
-
 def main (): 
     transform = ToTensor()
     train_set = MNIST(root='./../datasets', train=True, download = True, transform = transform)
@@ -25,6 +23,51 @@ def main ():
     test_loader = DataLoader(test_set, shuffle=False, batch_size=128)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = Vit()
+    model = Vit((1, 28, 28), n_patches=7, n_heads=2, n_blocks=2, hidden_d=2, out_d=10)
+    N_EPOCH = 5
+    LR = 0.005
+
+    # Traning loop 
+    optimizer = Adam(model.parameters(), lr=LR)
+    criterion =CrossEntropyLoss()
+    for epoch in trange(N_EPOCH, desc='Training'):
+        train_loss = 0.0
+        for batch in tqdm(train_loader, desc=f'Epoch {epoch+1} in training'):
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+
+            train_loss += loss.detach().cpu().items() / len(train_loader)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f'Epoch {epoch +1}/ {N_EPOCH} loss: {train_loss:.2f}')
+    
+    # Test
+    with torch.no_grad:
+        correct, total = 0, 0
+        test_loss = 0.0
+        for batch in tqdm(test_loader, desc='Testing'):
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+            test_loss += loss.detach().cpu().items() / len(test_loader)
+            correct += torch.sum(torch.argmax(y_hat, dim=1) == y).detach().cpu().item()
+
+            total += len(x)
+
+        print(f'test loss: {test_loss:.2f}')
+
+
+
+
+
+
+
+    
 
     
--- vit.py
+++ vit.py
@@ -3,17 +3,16 @@
 import numpy as np
 import torch
 import torch.nn as nn
-
 from tqdm import tqdm, trange
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
-
 from torchvision.transforms import ToTensor
 from torchvision.datasets.mnist import MNIST
 
 np.random.seed(0)
 torch.manual_seed(0)
+
 
 class Vit(nn.Module):
     def __init__(
@@ -22,7 +21,7 @@ class Vit(nn.Module):
             n_patches: int = 7,
             n_heads: int = 2,
             n_blocks: int = 2,
-            hidden_d: int = 2,
+            hidden_d: int = 8,
             out_d: int = 10
 
 
@@ -39,14 +38,21 @@ class Vit(nn.Module):
         assert chw[2] % n_patches == 0, "input shape should be divisible by n_patches"
 
         self.patch_size = (chw[1] / n_patches, chw[2] / n_patches) # 4 * 4
+
+        # add linear mapper 
         self.input_d = int(chw[0] * self.patch_size[0] * self.patch_size[1]) # 1 * 4 * 4 = 16
         self.linear_mapper = nn.Linear(self.input_d, self.hidden_d)
 
         # add learnable classification token
         self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
         # add positional embedding
-        self.pos_embedding = nn.Parameter(torch.tensor(self.n_patches ** 2 + 1, self.hidden_d))
-        self.pos_embedding.requires_grad = False
+       #self.pos_embedding = nn.Parameter(torch.tensor(self.n_patches ** 2 + 1, self.hidden_d))
+       #self.pos_embedding.requires_grad = False
+        self.register_buffer(
+            "positional_embeddings",
+            get_positional_embedding(n_patches**2+1, hidden_d),
+            persistent=False,
+        )
 
         # Encoder block
         self.block = nn.ModuleList(
@@ -60,11 +66,19 @@ class Vit(nn.Module):
         )
         
     def forward (self, images):
+        #divide images into patches
         n, c, h, w = images
-        patches = patch_embedding(images, self.n_patches)  # n, 49, 16
+        patches = patch_embedding(images, self.n_patches).to(self.positional_embeddings.device) # n, 49, 16
+
+        # Running linear layer tokenization
+        # Map the vector corresponding to each patch to the hidden size dimension
         tokens = self.linear_mapper(patches) # n, 49, 8
-        tokens = torch.stack([torch.vstack((self.class_token, tokens[i])) for i in range(len(tokens))])  # n, 50, 8
-        pos_embed = self.pos_embedding.repeat(n, 1, 1) # n, 50, 8
+
+        # Adding classification token to the tokens
+        tokens = torch.cat((self.class_token.expand(n, 1, -1), tokens), dim=1)
+        #tokens = torch.stack([torch.vstack((self.class_token, tokens[i])) for i in range(len(tokens))])  # n, 50, 8
+
+        pos_embed = self.positional_embeddings.repeat(n, 1, 1) # n, 50, 8
         out = tokens + pos_embed 
 
         #transformer block
@@ -100,7 +114,7 @@ def patch_embedding(images, n_patches):
     return patches  # n, 49, 16
 
 
-def positional_embedding(sequence_lenght, d): # 50, 8
+def get_positional_embedding(sequence_lenght, d): # 50, 8
     result = torch.ones(sequence_lenght, d)
     for i in range (sequence_lenght):
         for j in range (d):
@@ -112,34 +126,34 @@ class MultiHeadAttention (nn.module):
     def __init__(
             self,
             d,
-            n_heads):
+            n_heads=2):
         super(MultiHeadAttention, self).__init__()
 
         self.d = d
         self.n_heads = n_heads
 
-        assert d % n_heads == 0, f"Dimension {d} is not divisible by head: {n_heads}"
+        assert d % n_heads == 0, f"can't divide dmension {d} into : {n_heads} heads"
 
-        d_head = int(d/n_heads)
-        self.q = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-        self.k = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-        self.v = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        d_head = int(d / n_heads)
+        self.q_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.k_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.v_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
 
         self.d_head= d_head
-        self.softmax = nn.softmax(dim=1)
+        self.softmax = nn.softmax(dim=-1)
 
     def forward (self, sequences):
-        # n , seq_length, token_dim, #n, 50, 8/d_heads
-        # patch 8/head, each patch will have q, k, v
-
+        # Sequences has shape (N, seq_length, token_dim)
+        # We go into shape    (N, seq_length, n_heads, token_dim / n_heads)
+        # And come back to    (N, seq_length, item_dim)  (through concatenation)
         result = []
         for sequence in sequences:
             seq_result = []
             for head in range (self.n_heads): # 0, 1
-                q_mapping = self.q[head]
-                k_mapping = self.k[head]
-                v_mapping = self.v[head]
-                #watchout for possible error here
+                q_mapping = self.q_mappings[head]
+                k_mapping = self.k_mappings[head]
+                v_mapping = self.v_mappings[head]
+                
                 seq = sequence[:, head * self.d_head: (head+1) * self.d_head ] # take all rows, then begin from col 0:4, then col 4: 8. the shape: 50, 4
                 q, k, v = q_mapping(seq), k_mapping(seq), v_mapping(seq)
 
@@ -148,7 +162,7 @@ class MultiHeadAttention (nn.module):
 
             result.append(torch.hstack(seq_result))
 
-        return torch.cat(torch.unsqueeze(r, dim=0) for r in result) # n, 50, 8
+        return torch.cat([torch.unsqueeze(r, dim=0) for r in result]) # n, 50, 8
             
 
 # Encoder Block
